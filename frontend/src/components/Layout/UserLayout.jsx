f7c8f32497759877629ad8af5280a6a86739624d
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-06-30 08:25:48 +0530

    Initial setup

---
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const UserLayout = () => {
+  return (
+    <>
+      {/* Header */}
+      {/* Manin Content */}
+      {/* Footer */}
+    </>
+  );
+};
+
+export default UserLayout;
