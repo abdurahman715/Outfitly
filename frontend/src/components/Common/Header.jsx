41bafac863a7fb232c92886203fe37fac8f24089
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-06-30 14:38:17 +0530

    feat:Add Navbar and SerachBar

--- frontend/src/components/Common/Header.jsx
+++ frontend/src/components/Common/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import TopBar from "../Layout/TopBar";
+import Navbar from "./Navbar";
 
 const Header = () => {
   return (
-    <header>
+    <header className="border-b border-gray-200">
       {/* TopBar */}
       <TopBar />
       {/* NavBar */}
+      <Navbar />
       {/* Cart drawer */}
     </header>
   );
