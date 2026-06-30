b6a556d0b6974bd73aac8bbc3a9ff237c2147e11
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-06-30 09:44:39 +0530

    feat:created topbar

---
+++ frontend/src/components/Common/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import TopBar from "../Layout/TopBar";
+
+const Header = () => {
+  return (
+    <header>
+      {/* TopBar */}
+      <TopBar />
+      {/* NavBar */}
+      {/* Cart drawer */}
+    </header>
+  );
+};
+
+export default Header;
--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "../Common/Header";
 
 const UserLayout = () => {
   return (
     <>
       {/* Header */}
+      <Header />
       {/* Manin Content */}
       {/* Footer */}
     </>
