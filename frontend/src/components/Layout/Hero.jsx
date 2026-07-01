bb5e490b19c2f460742c7adac9c000f203fcf699
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-07-01 11:29:06 +0530

    feat:created Hero section of the home page and route

---
+++ frontend/src/components/Layout/Hero.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import heroImg from "../../assets/rabbit-hero.webp";
+const Hero = () => {
+  return (
+    <section className="relative">
+      <img
+        src={heroImg}
+        alt="hero image"
+        className="w-full h-[400px] md:h-[600px] lg:h-[750px] object-cover"
+      />
+
+      <div className="absolute inset-0 bg-black/20 flex flex-col items-center justify-center">
+        <div className="text-center text-white p-6">
+          <h1 className="text-4xl md:text-9xl font-bold tracking-tighter uppercase mb-4">
+            Vacation <br /> Ready
+          </h1>
+        </div>
+        <p className="text-white text-sm tracking-tighter md:text-lg mb-6">
+          Explore our vacation ready outfits with fast world wide shipping.
+        </p>
+        <Link
+          to="#"
+          className="bg-white text-gray-950 px-6 py-2 rounded-sm text-lg"
+        >
+          Shop Now
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
